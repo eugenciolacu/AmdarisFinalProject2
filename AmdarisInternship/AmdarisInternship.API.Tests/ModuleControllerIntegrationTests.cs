db6c096f72b2ca31b5604da6aefb20b3eb3c7671
Eugen <eugenciolacu@gmail.com>
2020-12-15 17:17:59 +0200

    15.12.2020 3rd Commit

---
+++ AmdarisInternship/AmdarisInternship.API.Tests/ModuleControllerIntegrationTests.cs
@@ -0,0 +1,123 @@
+﻿using AmdarisInternship.API.Dtos.ModuleDtos;
+using AmdarisInternship.API.Dtos.ModuleGradingDtos;
+using AmdarisInternship.API.Dtos.ModuleWithModuleGradingTdos;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace AmdarisInternship.API.Tests
+{
+    [TestClass]
+    public class ModuleControllerIntegrationTests
+    {
+        private readonly HttpClient _client;
+
+        public ModuleControllerIntegrationTests()
+        {
+            var appFactory = new WebApplicationFactory<Startup>();
+            _client = appFactory.CreateClient();
+        }
+
+        [TestMethod]
+        public async Task ShouldAccess_Get()
+        {
+            var request = "/api/Module";
+            var response = await _client.GetAsync(request);
+            response.EnsureSuccessStatusCode();
+            var jsonFromPostResponse = await response.Content.ReadAsStringAsync();
+            var obj = JsonConvert.DeserializeObject<List<ModuleWithModuleGradingDto>>(jsonFromPostResponse);
+            Assert.IsTrue(obj.Count == 6);
+        }
+
+        [TestMethod]
+        public async Task ShouldAccess_GetById()
+        {
+            var request = "/api/Module/1";
+            var response = await _client.GetAsync(request);
+            if (response.IsSuccessStatusCode)
+            {
+                var jsonFromPostResponse = await response.Content.ReadAsStringAsync();
+                var obj = JsonConvert.DeserializeObject<ModuleWithModuleGradingDto>(jsonFromPostResponse);
+                Assert.AreEqual(obj.Module.Name, "Module 1");
+                Assert.IsTrue(obj.ModuleGradings.Count == 2);
+            }
+            response.EnsureSuccessStatusCode();
+        }
+
+        [TestMethod]
+        public async Task ShouldAccess_Post()
+        {
+            var request = new
+            {
+                Url = "/api/Module",
+                Body = new
+                {
+                    module = new ModuleDto
+                    {
+                        Name = "Test"
+                    },
+
+                    moduleGradings = new List<ModuleGradingDto>
+                    {
+                        new ModuleGradingDto
+                        {
+                            Name = "Quiz 1",
+                            Weight = 0.5f,
+                            ModuleId = 10
+                        },
+                        new ModuleGradingDto
+                        {
+                            Name = "Quiz 2",
+                            Weight = 0.5f,
+                            ModuleId = 10
+                        }
+                    }
+                }
+            };
+
+            var postResponse = await _client.PostAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            postResponse.EnsureSuccessStatusCode();
+        }
+
+        [TestMethod]
+        public async Task ShouldAccess_Put()
+        {
+            var request = new
+            {
+                Url = "/api/Module/1",
+                Body = new
+                {
+                    module = new ModuleDto
+                    {
+                        Id = 1,
+                        Name = "xxxxx"
+                    },
+
+                    moduleGradings = new List<ModuleGradingDto>
+                    {
+                        new ModuleGradingDto
+                        {
+                            Id = 1,
+                            Name = "Quiz 1",
+                            Weight = 0.5f,
+                            ModuleId = 1
+                        },
+                        new ModuleGradingDto
+                        {
+                            Id = 2,
+                            Name = "Quiz 2",
+                            Weight = 0.5f,
+                            ModuleId = 1
+                        }
+                    }
+                }
+            };
+
+            var putResponse = await _client.PutAsync(request.Url, ContentHelper.GetStringContent(request.Body));
+            putResponse.EnsureSuccessStatusCode();
+        }
+    }
+}
---
+++ AmdarisInternship/AmdarisInternship.API.Tests/ModuleControllerUnitTests.cs
@@ -0,0 +1,245 @@
+﻿using AmdarisInternship.API.Controllers;
+using AmdarisInternship.API.Dtos.ModuleDtos;
+using AmdarisInternship.API.Dtos.ModuleGradingDtos;
+using AmdarisInternship.API.Dtos.ModuleWithModuleGradingTdos;
+using AmdarisInternship.API.Services.Interfaces;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+
+namespace AmdarisInternship.API.Tests
+{
+    [TestClass]
+    public class ModuleControllerUnitTests
+    {
+        private IList<ModuleWithModuleGradingDto> _moduleWithModuleGradingsList;
+
+        private Mock<IModuleModuleGradingsService> _mockModuleModuleGradingsService;
+
+        private ModuleController _moduleController;
+
+        private Mock<IMapper> _mockMapper;
+
+
+        [TestInitialize]
+        public void Initializer()
+        {
+            _mockModuleModuleGradingsService = new Mock<IModuleModuleGradingsService>();
+
+            _mockMapper = new Mock<IMapper>();
+
+            _moduleController = new ModuleController(_mockModuleModuleGradingsService.Object, _mockMapper.Object);
+
+            _moduleWithModuleGradingsList = new List<ModuleWithModuleGradingDto>
+            {
+                new ModuleWithModuleGradingDto
+                {
+                    Module = new ModuleDto
+                    {
+                        Id = 1,
+                        Name = "Module 1 Test"
+                    },
+
+                    ModuleGradings = new List<ModuleGradingDto>
+                    {
+                        new ModuleGradingDto
+                        {
+                            Id = 1,
+                            Name = "Quiz 1 Test",
+                            Weight = 0.5f,
+                            ModuleId = 1
+                        },
+
+                        new ModuleGradingDto
+                        {
+                            Id = 2,
+                            Name = "Quiz 2 Test",
+                            Weight = 0.5f,
+                            ModuleId = 1
+                        }
+                    }
+                },
+
+                new ModuleWithModuleGradingDto
+                {
+                    Module = new ModuleDto
+                    {
+                        Id = 2,
+                        Name = "Module 2 Test"
+                    },
+
+                    ModuleGradings = new List<ModuleGradingDto>
+                    {
+                        new ModuleGradingDto
+                        {
+                            Id = 3,
+                            Name = "Theory 1 Test",
+                            Weight = 0.1f,
+                            ModuleId = 2
+                        },
+
+                        new ModuleGradingDto
+                        {
+                            Id = 4,
+                            Name = "Theory 2 Test",
+                            Weight = 0.1f,
+                            ModuleId = 2
+                        },
+
+                        new ModuleGradingDto
+                        {
+                            Id = 5,
+                            Name = "Practice Test",
+                            Weight = 0.6f,
+                            ModuleId = 2
+                        },
+
+                        new ModuleGradingDto
+                        {
+                            Id = 6,
+                            Name = "Quiz Test",
+                            Weight = 0.2f,
+                            ModuleId = 2
+                        }
+                    }
+                },
+
+                new ModuleWithModuleGradingDto
+                {
+                    Module = new ModuleDto
+                    {
+                        Id = 3,
+                        Name = "Module 1 Test"
+                    },
+
+                    ModuleGradings = new List<ModuleGradingDto>
+                    {
+                        new ModuleGradingDto
+                        {
+                            Id = 7,
+                            Name = "Quiz Test",
+                            Weight = 1f,
+                            ModuleId = 3
+                        }
+                    }
+                }
+            };
+        }
+
+        [TestMethod]
+        public void GetTest()
+        {
+            var expected = _moduleWithModuleGradingsList;
+            _mockModuleModuleGradingsService.Setup(x => x.GetModulesWithModuleGradings()).Returns(expected);
+
+            var modulesWithModuleGradings = _moduleController.Get() as OkObjectResult;
+            var result = modulesWithModuleGradings.Value as List<ModuleWithModuleGradingDto>;
+
+            Assert.AreEqual(modulesWithModuleGradings.StatusCode, (int)HttpStatusCode.OK);
+            Assert.AreEqual(result.Count(), 3);
+            Assert.AreEqual(result[0].ModuleGradings.Count(), 2);
+            Assert.AreEqual(result[1].ModuleGradings.Count(), 4);
+            Assert.AreEqual(result[2].ModuleGradings.Count(), 1);
+        }
+
+        [TestMethod]
+        [DataRow(1)]
+        public void GetTestWithId(int id)
+        {
+            var expected = _moduleWithModuleGradingsList.FirstOrDefault(x => x.Module.Id == id);
+            _mockModuleModuleGradingsService.Setup(x => x.GetModuleWithModuleGradingsByModuleId(It.IsAny<int>())).Returns(expected);
+
+            var modulesWithModuleGradings = _moduleController.Get(id) as OkObjectResult;
+            var result = modulesWithModuleGradings.Value as ModuleWithModuleGradingDto;
+
+            Assert.AreEqual(modulesWithModuleGradings.StatusCode, (int)HttpStatusCode.OK);
+            Assert.AreEqual(result.Module.Name, _moduleWithModuleGradingsList[0].Module.Name);
+            Assert.AreEqual(result.ModuleGradings.Count, _moduleWithModuleGradingsList[0].ModuleGradings.Count);
+            Assert.AreEqual(result.ModuleGradings[0].Name, _moduleWithModuleGradingsList[0].ModuleGradings[0].Name);
+            Assert.AreEqual(result.Module.Id, _moduleWithModuleGradingsList[0].ModuleGradings[0].ModuleId);
+        }
+
+        [TestMethod]
+        public void PostTest()
+        {
+            var newModuleWithModuleGrading = new ModuleWithModuleGradingDto
+            {
+                Module = new ModuleDto
+                {
+                    Id = 4,
+                    Name = "Module 4 Test"
+                },
+
+                ModuleGradings = new List<ModuleGradingDto>
+                {
+                    new ModuleGradingDto
+                    {
+                        Id = 8,
+                        Name = "Quiz Test",
+                        Weight = 1f,
+                        ModuleId = 4
+                    }
+                }
+            };
+
+            _mockModuleModuleGradingsService.Setup(x => x.AddNewModuleWithModuleGrading(It.IsAny<ModuleWithModuleGradingDto>())).Returns(newModuleWithModuleGrading);
+
+            var moduleWithModuleGradings = _moduleController.Post(newModuleWithModuleGrading) as ObjectResult;
+            var result = moduleWithModuleGradings.Value as ModuleWithModuleGradingDto;
+
+            Assert.AreEqual(moduleWithModuleGradings.StatusCode, (int)HttpStatusCode.Created);
+            Assert.AreEqual(newModuleWithModuleGrading.Module.Name, result.Module.Name);
+            Assert.AreEqual(newModuleWithModuleGrading.ModuleGradings.Count, result.ModuleGradings.Count);
+            Assert.AreEqual(newModuleWithModuleGrading.ModuleGradings[0].Name, result.ModuleGradings[0].Name);
+            Assert.AreEqual(newModuleWithModuleGrading.ModuleGradings[0].ModuleId, result.Module.Id);
+        }
+
+        [TestMethod]
+        [DataRow(2)]
+        public void PutTest(int id)
+        {
+            var moduleWithModuleGradingToUpdate = new ModuleWithModuleGradingDto
+            {
+                Module = new ModuleDto
+                {
+                    Id = 2,
+                    Name = "xxxxxxxxx"
+                },
+
+                ModuleGradings = new List<ModuleGradingDto>
+                {
+                    new ModuleGradingDto
+                    {
+                        Id = 3,
+                        Name = "Quiz 1",
+                        Weight = 0.5f,
+                        ModuleId = 2
+                    },
+
+                    new ModuleGradingDto
+                    {
+                        Id = 0,
+                        Name = "Quiz 2",
+                        Weight = 0.5f,
+                        ModuleId = 2
+                    }
+                }
+            };
+
+            _mockModuleModuleGradingsService.Setup(x => x.UpdateModuleWithModuleGrading(id, It.IsAny<ModuleWithModuleGradingDto>())).Returns(moduleWithModuleGradingToUpdate);
+
+            var moduleWithModuleGradings = _moduleController.Put(id, moduleWithModuleGradingToUpdate) as ObjectResult;
+            var result = moduleWithModuleGradings.Value as ModuleWithModuleGradingDto;
+
+            Assert.AreEqual(moduleWithModuleGradings.StatusCode, (int)HttpStatusCode.Created);
+            Assert.AreEqual(moduleWithModuleGradingToUpdate.Module.Name, result.Module.Name);
+            Assert.AreEqual(moduleWithModuleGradingToUpdate.ModuleGradings.Count, result.ModuleGradings.Count);
+            Assert.AreEqual(moduleWithModuleGradingToUpdate.ModuleGradings[0].Name, result.ModuleGradings[0].Name);
+            Assert.AreEqual(moduleWithModuleGradingToUpdate.ModuleGradings[0].ModuleId, result.Module.Id);
+        }
+    }
+}
